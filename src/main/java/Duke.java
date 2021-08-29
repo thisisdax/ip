89d0f2e0a28fe49907f36d00b5fd0ffcfa148c41
Dax Tan <this.is.dax@gmail.com>
2021-08-29 23:52:57 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,10 +9,21 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello! I'm Duke");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________");
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            String cmd = input.nextLine();
+            System.out.println("\t____________________________________________________________");
+            if (cmd.toLowerCase().equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println("\t"+cmd);
+            System.out.println("\t____________________________________________________________");
+        }
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
     }
 }
